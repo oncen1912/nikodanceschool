ad0363f29d4c373297c7098ec012404a0293314d
oncen <nikolaj.coh@gmail.com>
2025-12-16 19:18:50 +0100

    minor

--- frontend/src/components/tabs/HomeTab.vue
+++ frontend/src/components/tabs/HomeTab.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-4 bg-light rounded">
     <h3>Welcome to Niko Dance School!</h3>
-    <p>You are ready to bounce anything.</p>
+    <p>You are ready to bounce anything...</p>
     <router-link to="/events" class="btn btn-primary">Go to events</router-link>
   </div>
 </template>
