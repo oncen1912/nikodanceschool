87010e97373dcf08fe338e8fe1feb46fcf8ecf93
oncen <nikolaj.coh@gmail.com>
2025-12-16 19:07:54 +0100

    deployment test

--- frontend/src/components/tabs/HomeTab.vue
+++ frontend/src/components/tabs/HomeTab.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-4 bg-light rounded">
-    <h3>Welcome Home!</h3>
-    <p>This is the home tab. You are ready to build anything.</p>
+    <h3>Welcome to Niko Dance School!</h3>
+    <p>You are ready to bounce anything.</p>
     <router-link to="/events" class="btn btn-primary">Go to events</router-link>
   </div>
 </template>
